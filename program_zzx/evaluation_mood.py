643f369a2fc7cfd25e63c13c780246df8fc2b00e
Josh <zhaoxia2@ualberta.ca>
2022-08-30 23:48:35 -0600

    nothing

--- program_zzx/evaluation_mood.py
+++ program_zzx/evaluation_mood.py
@@ -4,6 +4,27 @@ import cv2
 import numpy as np
 from sklearn.metrics import roc_auc_score, average_precision_score
 
+def get_data_transforms(size, isize):
+    # mean_train = [0.485]         # how do you set the mean_train and std_train in the get_data_transforms function?
+    # mean_train = [-0.1]
+    # std_train = [0.229]
+    data_transforms = transforms.Compose([
+        # transforms.Resize((size, size)),
+        # transforms.CenterCrop(isize),
+        
+        #transforms.CenterCrop(args.input_size),
+        transforms.ToTensor()
+        # transforms.Normalize(mean=mean_train,
+        #                      std=std_train)
+    ])
+    gt_transforms = transforms.Compose([
+        transforms.Resize((size, size)),
+        transforms.CenterCrop(isize),
+        transforms.ToTensor()])
+
+    return data_transforms, gt_transforms
+
+
 
 def mean(list_x):
     return sum(list_x)/len(list_x)
@@ -127,3 +148,100 @@ def evaluation(args, model, test_dataloader, epoch, loss_l1, visualizer, run_nam
     print('Pixel Average Precision:{:.4f}, Sample Average Precision:{:.4f}'.format(pixelAP, sampleAP))
     with open(result_path, 'a') as f:
         f.writelines('Pixel Average Precision:{:.4f}, Sample Average Precision:{:.4f}'.format(pixelAP, sampleAP))
+        
+        
+        
+        
+if __name__ == '__main__':
+    import argparse
+    from model_noise import UNet
+    
+    from dataloader_zzx import MVTecDataset
+    from torch.utils.data import DataLoader
+    from tensorboard_visualizer import TensorboardVisualizer
+    
+    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--obj_id', default=1,  action='store', type=int)
+    parser.add_argument('--lr', default=0.0001, action='store', type=float)
+    parser.add_argument('--epochs', default=700, action='store', type=int)
+    parser.add_argument('--c v', default='/home/zhaoxiang/baselines/DRAEM/datasets/mvtec/', action='store', type=str)
+    parser.add_argument('--anomaly_source_path', default='/home/zhaoxiang/baselines/DRAEM/datasets/dtd/images/', action='store', type=str)
+    parser.add_argument('--checkpoint_path', default='./checkpoints/', action='store', type=str)
+    parser.add_argument('--log_path', default='./logs/', action='store', type=str)
+    parser.add_argument('--visualize', default=True, action='store_true')
+
+    parser.add_argument('--backbone', default='noise', action='store')
+    
+    # for noise autoencoder
+    parser.add_argument("-nr", "--noise_res", type=float, default=16,  help="noise resolution.")
+    parser.add_argument("-ns", "--noise_std", type=float, default=0.2, help="noise magnitude.")
+    parser.add_argument("-img_size", "--img_size", type=float, default=256, help="noise magnitude.")
+    
+    # need to be changed/checked every time
+    parser.add_argument('--bs', default = 1, action='store', type=int)
+    parser.add_argument('--gpu_id', default = ['0','1'], action='store', type=str, required=False)
+    parser.add_argument('--experiment_name', default='mood_cv2', choices=['retina, liver, brain, head', 'chest'], action='store')
+    parser.add_argument('--dataset_name', default='Mood_brain_cv2', choices=['hist_DIY', 'Brain_MRI', 'Head_CT', 'CovidX', 'RESC_average'], action='store')
+    parser.add_argument('--model', default='ws_skip_connection', choices=['ws_skip_connection', 'DRAEM_reconstruction', 'DRAEM_discriminitive'], action='store')
+    parser.add_argument('--process_method', default='Multi_randomShape_wo_distortion', choices=['none', 'Gaussian_noise', 'DRAEM_natural', 'DRAEM_tumor', 'Simplex_noise', 'Simplex_noise_best_best'], action='store')
+    parser.add_argument('--resume_training', default = True, action='store', type=int)
+    
+    parser.add_argument('--augmentation', default = 'cp', action='store', type=int)
+    
+    args = parser.parse_args()
+
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    if args.gpu_id is None:
+        gpus = "0"
+        os.environ["CUDA_VISIBLE_DEVICES"]= gpus
+    else:
+        gpus = ""
+        for i in range(len(args.gpu_id)):
+            gpus = gpus + args.gpu_id[i] + ","
+        os.environ["CUDA_VISIBLE_DEVICES"]= gpus[:-1]
+
+    torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
+    
+    data_transform, gt_transform = get_data_transforms(args.img_size, args.img_size)
+    main_path = '/home/zhaoxiang/dataset/{}'.format(args.dataset_name)
+    
+    dirs = os.listdir(main_path)
+    
+    for dir_name in dirs:
+        if 'train' in dir_name:
+            train_dir = dir_name
+        elif 'test_{}'.format(args.augmentation) in dir_name:
+            if 'label' in dir_name:
+                label_dir = dir_name
+            else:
+                test_dir = dir_name
+    if 'label_dir' in locals():
+        dirs = [train_dir, test_dir, label_dir]                
+
+    
+    
+    device = None
+    n_input = 1
+    n_classes = 1           # the target is the reconstructed image
+    depth = 4
+    wf = 6
+    
+    loss_l1 = torch.nn.L1Loss()
+    visualizer = TensorboardVisualizer(log_dir=os.path.join(args.log_path, run_name+"/"))
+    
+    model = UNet(in_channels=n_input, n_classes=n_classes, norm="group", up_mode="upconv", depth=depth, wf=wf, padding=True).cuda()
+    model = torch.nn.DataParallel(model, device_ids=[0, 1])
+    
+    val_data = MVTecDataset(root=main_path, transform = data_transform, gt_transform=gt_transform, phase='test', dirs = dirs, data_source=args.experiment_name)
+    val_dataloader = torch.utils.data.DataLoader(val_data, batch_size = args.bs, shuffle = False)
+    
+    run_name = args.experiment_name + '_' +str(args.lr)+'_'+str(args.epochs)+'_bs'+str(args.bs)+"_" + args.model + "_" + args.process_method
+    
+    
+    epoch = 'test'
+    
+    with torch.no_grad():
+        model.eval()
+        evaluation(args, model, val_dataloader, epoch, loss_l1, visualizer, run_name)
